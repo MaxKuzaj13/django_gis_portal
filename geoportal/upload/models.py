4da02ea8027f75f348e47a37d77a2bf6f63908e3
rogowskm <maksymilian.rogowski@tomtom.com>
2021-09-15 12:30:32 +0200

    Create db UploadFile and Raport models

---
+++ geoportal/upload/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+TYPE_GEOMETRY = (
+    ('line', 'Line feature'),
+    ('point', 'Point feature'),
+    ('polygon', 'Polygon feature')
+        )
+
+class UploadFile(models.Model):
+    user_name = models.CharField(max_length=255)
+    file = models.CharField(max_length=255)
+    layer = models.CharField(max_length=255)
+    type = models.CharField(choices=TYPE_GEOMETRY, default='None', max_length=50)
+    link_to_file = models.FileField(upload_to='.')
+
+class Raport(models.Model):
+    # delete rap if delete org file (models.Cascade)
+    upload_file = models.ForeignKey(UploadFile, on_delete=models.CASCADE)
+    upload_link = models.FileField(upload_to='.')
+
