50d7fc6fc332d8ad3a9e97f8a4a61130c00aa718
rogowskm <maksymilian.rogowski@tomtom.com>
2021-09-15 12:31:49 +0200

    Speling error change name to report

--- geoportal/upload/models.py
+++ geoportal/upload/models.py
@@ -4,7 +4,8 @@ TYPE_GEOMETRY = (
     ('line', 'Line feature'),
     ('point', 'Point feature'),
     ('polygon', 'Polygon feature')
-        )
+)
+
 
 class UploadFile(models.Model):
     user_name = models.CharField(max_length=255)
@@ -13,8 +14,8 @@ class UploadFile(models.Model):
     type = models.CharField(choices=TYPE_GEOMETRY, default='None', max_length=50)
     link_to_file = models.FileField(upload_to='.')
 
-class Raport(models.Model):
+
+class Report(models.Model):
     # delete rap if delete org file (models.Cascade)
     upload_file = models.ForeignKey(UploadFile, on_delete=models.CASCADE)
     upload_link = models.FileField(upload_to='.')
-
